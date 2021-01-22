d53030f6db4e2129120ff8acd4b53e1edd7dbd8e
Leandro <leandrotux@hotmail.com>
2021-01-22 14:36:04 -0300

    first commit clone youtube add button

---
+++ lib/Home.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/material.dart';
+
+class Home extends StatefulWidget {
+  @override
+  _HomeState createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        //backgroundColor: Colors.black,
+        title: Text("Youtube"),
+        actions: [
+          IconButton(
+            icon: Icon(Icons.videocam),
+            onPressed: () {
+              print("acao: videocam");
+            },
+          ),
+          IconButton(
+            icon: Icon(Icons.search),
+            onPressed: () {
+              print("acao: pesquisa");
+            },
+          ),
+          IconButton(
+            icon: Icon(Icons.account_circle),
+            onPressed: () {
+              print("acao: conta");
+            },
+          )
+        ],
+      ),
+      body: Container(),
+    );
+  }
+}
